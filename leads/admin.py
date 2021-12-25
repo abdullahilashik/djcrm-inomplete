0974ea134241dbc284dd59e9b9afc6ceb5dd4082
Ashik Arefin <abdullahilashik@gmail.com>
2021-12-25 21:58:11 +0600

    sync

--- leads/admin.py
+++ leads/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Agent, Lead
 
-# Register your models here.
+
+@admin.register(Lead)
+class LeadAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'last_name', 'age', 'agent']
+
+
+@admin.register(Agent)
+class AgentAdmin(admin.ModelAdmin):
+    pass
--- leads/models.py
+++ leads/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+
 class CustomUser(AbstractUser):
     phone = models.CharField(max_length=50)
 
     def __str__(self):
         return self.username
+
 
 class Lead(models.Model):
     first_name = models.CharField(max_length=250)
@@ -13,7 +15,12 @@ class Lead(models.Model):
     age = models.IntegerField()
     agent = models.ForeignKey('Agent', on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.first_name
+
+
 class Agent(models.Model):
-    first_name = models.CharField(max_length=250)
-    last_name = models.CharField(max_length=250)
-    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.username
